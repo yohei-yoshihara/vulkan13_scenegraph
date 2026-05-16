583b95948fbf8479c016559ab7c6ad6676afd4f6
Yohei Yoshihara <yy2.0101@gmail.com>
2026-05-17 07:54:35 +0900

    replace doctest with catch2

--- appTests/test1.cpp
+++ appTests/test1.cpp
@@ -1,5 +1,8 @@
-#include "doctest.h"
+#include <catch2/catch_test_macros.hpp>
 
 int add(int a, int b) { return a + b; }
 
-TEST_CASE("add test") { CHECK(add(1, 2) == 3); }
+TEST_CASE("add test") { 
+  // sample test case
+  CHECK(add(1, 2) == 3);
+}
